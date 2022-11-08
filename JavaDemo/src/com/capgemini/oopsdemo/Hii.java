765ce166bbb4dc9749db48b9e5aa7711e56dce5e
shubham <shubham@aaa>
2022-11-08 16:08:00 +0530

    third commit

---
+++ JavaDemo/src/com/capgemini/oopsdemo/Hii.java
@@ -0,0 +1,10 @@
+package com.capgemini.oopsdemo;
+
+public class Hii {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         System.out.println("hii there");
+	}
+
+}
