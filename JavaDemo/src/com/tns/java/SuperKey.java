d5bed1b2abc0ccb9caf79eab2de3cd0c04529765
shubham <shubham@aaa>
2022-11-08 22:13:45 +0530

    commit5

---
+++ JavaDemo/src/com/tns/java/SuperKey.java
@@ -0,0 +1,13 @@
+package com.tns.java;
+class Animal{
+	String color = "white";
+}
+
+public class SuperKey {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
