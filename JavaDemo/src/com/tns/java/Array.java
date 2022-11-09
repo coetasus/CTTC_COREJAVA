42c2b17f160a869e78b0719ac3fc468fd8fd8a37
shubham <shubham@aaa>
2022-11-09 19:01:50 +0530

    commit 6

---
+++ JavaDemo/src/com/tns/java/Array.java
@@ -0,0 +1,21 @@
+package com.tns.java;
+
+public class Array {
+
+	public static void main(String[] args) {
+		int arr[]=new int[5];
+		
+		arr[0]=4;
+		arr[1]=8;
+		arr[2]=9;
+		arr[3]=2;
+		arr[4]=6;
+		
+		for(int i=0; i<5; i++) {
+			System.out.println(arr[i]);
+		}
+		
+
+	}
+
+}
